00cfddba85cc1cf1234bd6f46a898a62939e161a
Nirban-A <itsnabrina@gmail.com>
2026-04-24 22:36:55 +0530

    first working view count on the site

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -11,3 +11,14 @@ script.dataset.goatcounter = 'https://python-oppe.goatcounter.com/count';
 script.async = true;
 script.src = '//gc.zgo.at/count.js';
 document.head.appendChild(script);
+
+// 2. Fetch JSON Count and Display as Text
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('https://python-oppe.goatcounter.com/counter/TOTAL.json')
+        .then(response => response.json())
+        .then(data => {
+            const el = document.getElementById('gc-stats');
+            if (el) el.innerText = "👀 " + data.count + " Views";
+        })
+        .catch(err => console.log("Stats blocked by privacy setting."));
+});
